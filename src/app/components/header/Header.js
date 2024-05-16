9a5b80fc9a719370b0b53ce880804496f2f606e3
jaydippatel83 <jaydippatel83.pj@gmail.com>
2024-05-16 18:33:31 +0530

    sidebar close btn

--- src/app/components/header/Header.js
+++ src/app/components/header/Header.js
@@ -1,6 +1,5 @@
 'use client';
-import React, { useEffect, useRef, useState } from 'react';
-import DarkModeComponent from '../theme/Dark';
+import React from 'react'; 
 import Link from 'next/link';
 import Image from 'next/image';
 import { IonIcon } from '@ionic/react';
@@ -9,28 +8,21 @@ import DropDown from './search/DropDown';
 import Create from './create/Create';
 import Notifications from './notification/Notifications';
 import Message from './message/Message';
-import Profile from './user/Profile';
+import Profile from './user/Profile'; 
 
-
-const Header = () => {
-    const [isSidebarOpen, setIsSidebarOpen] = useState(false);
-
-    const toggleSidebar = () => {
-        setIsSidebarOpen(!isSidebarOpen);
-    };
-
+const Header = ({isSidebarOpen,toggleSidebar}) => { 
     return (
         <header className="z-[100] h-[--m-top] fixed top-0 left-0 w-full flex items-center bg-white/80 sky-50 backdrop-blur-xl border-b border-slate-200 dark:bg-dark2 dark:border-slate-800">
             <div className="flex items-center w-full xl:px-6 px-2 max-lg:gap-10">
                 <div className="2xl:w-[--w-side] lg:w-[--w-side-sm]">
                     <div className="flex items-center gap-1">
                         <button
-                            uk-toggle="target: #site__sidebar ; cls :!-translate-x-0"
+                            onClick={toggleSidebar}
                             className="flex items-center justify-center w-8 h-8 text-xl rounded-full hover:bg-gray-100 xl:hidden dark:hover:bg-slate-600 group"
                         >
-                            <IonIcon icon={menuOutline} className="text-2xl group-aria-expanded:hidden" />
-                            <IonIcon icon={closeOutline} className="hidden text-2xl group-aria-expanded:block" />
-                        </button>
+                            <IonIcon icon={menuOutline} className={`text-2xl ${isSidebarOpen ? 'hidden' : 'block'}`} />
+                            <IonIcon icon={closeOutline} className={`text-2xl ${isSidebarOpen ? 'block' : 'hidden'}`} />
+                        </button> 
                         <div id="logo">
                             <Link href="/">
                                 <Image
@@ -70,7 +62,7 @@ const Header = () => {
                     <div className="max-w-[1220px] mx-auto flex items-center">
                         <DropDown />
                         <div className="flex items-center sm:gap-4 gap-2 absolute right-5 top-1/2 -translate-y-1/2 text-black">
-                            <button type="button" className="sm:hidden  sm:p-2 p-1 rounded-full relative sm:bg-secondery dark:text-white">
+                            <button type="button" className="sm:hidden sm:p-2 p-1 rounded-full relative sm:bg-secondery dark:text-white">
                                 <IonIcon icon={search} className="sm:hidden text-2xl" />
                             </button>
                             <Create />
--- src/app/components/layout/Layout.js
+++ src/app/components/layout/Layout.js
@@ -1,12 +1,19 @@
-import React from 'react';
+'use client';
+import React, { useState } from 'react';
 import Header from '../header/Header'; 
 import Sidebar from '../sidebar/Sidebar';
 
 const Layout = ({ children }) => {
+    const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+    const toggleSidebar = () => {
+        setIsSidebarOpen(!isSidebarOpen);
+    };
+    
     return (
         <div id='wrapper'>
-            <Header />
-            <Sidebar/>
+            <Header isSidebarOpen={isSidebarOpen} toggleSidebar={toggleSidebar}/>
+            <Sidebar isSidebarOpen={isSidebarOpen} toggleSidebar={toggleSidebar} />
             {children} 
         </div>
     );
