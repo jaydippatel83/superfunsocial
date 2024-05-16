d4ca4c9e4789bfba0e400bb21d5f0ce101dc7fcd
jaydippatel83 <jaydippatel83.pj@gmail.com>
2024-05-16 14:09:08 +0530

    Add main page design

--- src/app/components/layout/Layout.js
+++ src/app/components/layout/Layout.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import Header from '../header/Header';
 import Footer from '../footer/Footer';
+import Sidebar from '../sidebar/Sidebar';
 
 const Layout = ({ children }) => {
     return (
-        <>
+        <div id='wrapper'>
             <Header />
+            <Sidebar/>
             {children}
-            <Footer />
-        </>
+            {/* <Footer /> */}
+        </div>
     );
 };
 
