5305ce40114e8d125b81588c52a0f947f0aee399
jaydippatel83 <jaydippatel83.pj@gmail.com>
2024-05-17 14:48:33 +0530

    add right sidebar ceator list

--- src/app/page.js
+++ src/app/page.js
@@ -12,20 +12,16 @@ export default function Home() {
   return (
     <Layout>
       <main id="site__main" className="2xl:ml-[--w-side]  xl:ml-[--w-side-sm] p-2.5 h-[calc(100vh-var(--m-top))] mt-[--m-top]">
-
         <div className="lg:flex 2xl:gap-12 gap-8 max-w-[1065px] mx-auto" id="js-oversized"> 
-          <div className="max-w-[1080px] mx-auto flex-auto"> 
-
+          <div className="max-w-[1080px] mx-auto flex-auto">  
             <div className="md:max-w-[580px] mx-auto flex-1 xl:space-y-6 space-y-3"> 
               <CreatePost/>
               <PostCards/> 
               <PostCardLoader />
             </div>
           </div> 
-          <RightSIdeBar />
-
-        </div>
-
+          <RightSIdeBar /> 
+        </div> 
       </main>
     </Layout>
 
