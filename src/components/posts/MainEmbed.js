f9bf76b74c58c14c10d2cde1b5a63c519427369d
mansijoshi17 <mansijoshi1777@gmail.com>
2024-05-27 14:36:21 +0530

    Added changes in frame

--- src/components/posts/MainEmbed.js
+++ src/components/posts/MainEmbed.js
@@ -1,23 +1,36 @@
-import React from 'react';
-import RepostCast from './RepostCast';
-import EmbedUrls from './EmbedUrls';
-import Link from 'next/link';
+import React from "react";
+import RepostCast from "./RepostCast";
+import EmbedUrls from "./EmbedUrls";
+import Link from "next/link";
 
-const MainEmbed = ({ data ,lable}) => { 
-    return (
-        <Link href={`${data?.author?.username}/${data?.hash}`}> 
-            {data?.embeds && data.embeds.map((embed, index) => {
-                return <>
-                    {embed?.cast_id &&
-                        <RepostCast key={index} id={embed.cast_id.fid} hash={embed.cast_id.hash} classId="max-w-4xl mx-auto px-4" />
-                    }
-                    {
-                        embed.url &&  <EmbedUrls key={index} data={embed.url} lable="max-w-4xl mx-auto px-4"/>
-                    }
-                </>
-            })}
-        </Link>
-    );
+const MainEmbed = ({ data, lable }) => {
+  console.log(data, "MainEmbed");
+  return (
+    <Link href={`/${data?.author?.username}/${data?.hash}`}>
+      {data?.embeds &&
+        data.embeds.map((embed, index) => {
+          return (
+            <>
+              {embed?.cast_id && (
+                <RepostCast
+                  key={index}
+                  id={embed.cast_id.fid}
+                  hash={embed.cast_id.hash}
+                  classId="max-w-4xl mx-auto px-4"
+                />
+              )}
+              {embed.url && (
+                <EmbedUrls
+                  key={index}
+                  data={embed.url}
+                  lable="max-w-4xl mx-auto px-4"
+                />
+              )}
+            </>
+          );
+        })}
+    </Link>
+  );
 };
 
 export default MainEmbed;
