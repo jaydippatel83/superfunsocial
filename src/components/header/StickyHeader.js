36a0591629e6a01d45665615c9b3b3b3e209f3ad
mansijoshi17 <mansijoshi1777@gmail.com>
2024-06-26 16:28:34 +0530

    Added replies and likes tabs in profile

--- src/components/header/StickyHeader.js
+++ src/components/header/StickyHeader.js
@@ -1,20 +1,20 @@
-'use client';
-import { IonIcon } from '@ionic/react';
-import React, { useEffect, useState } from 'react';
-import PostCards from '../posts/PostCards';
-import PostCardLoader from '../loader/PostCardLoader';
-import PollInputForm from '../poll/PollForm';
-import CreatePost from '../posts/CreatePost';
-import { imageOutline, videocamOutline } from 'ionicons/icons';
-import { getFeed } from '@/lib/farcaster'; 
-import PullToRefresh from 'react-pull-to-refresh';
+"use client";
+import { IonIcon } from "@ionic/react";
+import React, { useEffect, useState } from "react";
+import PostCards from "../posts/PostCards";
+import PostCardLoader from "../loader/PostCardLoader";
+import PollInputForm from "../poll/PollForm";
+import CreatePost from "../posts/CreatePost";
+import { imageOutline, videocamOutline } from "ionicons/icons";
+import { getFeed } from "@/lib/farcaster";
+import PullToRefresh from "react-pull-to-refresh";
 
 const StickyHeader = ({ data, cursor }) => {
-  const [activeTab, setActiveTab] = useState('All');
+  const [activeTab, setActiveTab] = useState("All");
   const [showPollModal, setShowPollModal] = useState(false);
   const [casts, setCasts] = useState(data || []);
   const [loadcasts, setLoadcasts] = useState(false);
-  const [endCursorcasts, setEndCursorcasts] = useState(cursor); 
+  const [endCursorcasts, setEndCursorcasts] = useState(cursor);
   const [loader, setLoader] = useState(false);
 
   const handleTabClick = (tab) => {
@@ -26,12 +26,12 @@ const StickyHeader = ({ data, cursor }) => {
   };
 
   const loadmoreCastData = async () => {
-    setLoadcasts(true); 
-      const { feed } = await getFeed(endCursorcasts) 
-      setEndCursorcasts(feed?.next.cursor);
-      setCasts([...casts, ...feed?.casts]);
-      setLoadcasts(false); 
-  }; 
+    setLoadcasts(true);
+    const { feed } = await getFeed(endCursorcasts);
+    setEndCursorcasts(feed?.next.cursor);
+    setCasts([...casts, ...feed?.casts]);
+    setLoadcasts(false);
+  };
   const refreshData = async () => {
     setLoader(true);
     const { feed } = await getFeed(null); // null to get the latest data
@@ -42,98 +42,123 @@ const StickyHeader = ({ data, cursor }) => {
 
   useEffect(() => {
     // const interval = setInterval(() => {
-      refreshData();
+    refreshData();
     // },60000); // 60000 ms = 1 minute
 
     // Clear interval on component unmount
-  //   return () => clearInterval(interval);
-   }, []);
+    //   return () => clearInterval(interval);
+  }, []);
   return (
-    
-      <PullToRefresh onRefresh={refreshData}>
-        <div className="flex flex-col">
-          {
-            loader && <div className="flex justify-center align-middle h-20">
-              <div className="w-10 h-10 border-4 border-t-blue-500 border-solid rounded-full animate-spin"></div>
-            </div>
-          }
-      <div className="flex items-center justify-between mt-3 border-gray-100 px-2 max-lg:flex-col dark:border-slate-700 sticky top-16 border bg-white dark:bg-gray-800 z-50 my-5">
-        <nav className="flex gap-0.5 rounded-xl -mb-px text-gray-600 font-medium text-[15px] dark:text-white max-md:w-full max-md:overflow-x-auto">
-          {['All', 'Memes', 'Polls'].map((tab) => (
-            <button
-              key={tab}
-              onClick={() => handleTabClick(tab)}
-              className={`inline-block py-3 leading-8 px-3.5 ${activeTab === tab
-                ? 'border-b-2 border-blue-600 text-blue-600 dark:text-blue-400'
-                : 'text-gray-600 dark:text-gray-400'
-                }`}
-            >
-              {tab}
-            </button>
-          ))}
-        </nav>
-      </div>
-      {activeTab === 'All' && <>
-          <CreatePost />
-          {casts && casts.map((item,i) => (
-            <PostCards key={i} data={item} />
-          ))}
-          {endCursorcasts && (
-            <div className="flex justify-center my-5">
-              <button onClick={loadmoreCastData} 
-              className="inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
-              disabled={loadcasts}
-              >
-                {loadcasts ? <div className="w-8 h-8 border-4 border-t-blue-500 border-solid rounded-full animate-spin"></div> : "Load More"}
-              </button>
-            </div>
-          )}
-        </>}
-      {activeTab === 'Memes' && <>
-        <CreatePost />
-        {casts && casts.map((item,i) => (
-          <PostCards key={i} data={item} />
-        ))}
-        {endCursorcasts && (
-          <div className="flex justify-center">
-            <button onClick={()=>loadmoreCastData()} className="mt-4 font-bold text-base sm:text-lg bg-gradient-to-r text-white px-4 sm:px-6 py-1 sm:py-2 from-ct-blue-dark to-ct-blue-light min-w-[80px] sm:min-w-[100px] text-center rounded-md">
-              {loadcasts ? <div className="spinner-circular"></div> : "Load More"}
-            </button>
+    <PullToRefresh onRefresh={refreshData}>
+      <div className="flex flex-col">
+        {loader && (
+          <div className="flex justify-center align-middle h-20">
+            <div className="w-10 h-10 border-4 border-t-blue-500 border-solid rounded-full animate-spin"></div>
           </div>
         )}
-      </>}
-      {activeTab === 'Polls' && <>
-        <div className="bg-white rounded-xl shadow-sm md:p-4 p-2 space-y-4 text-sm font-medium border1 dark:bg-dark2">
-          <div className="flex items-center md:gap-3 gap-1">
-            <div
-              className="flex-1 bg-slate-100 hover:bg-opacity-80 transition-all rounded-lg cursor-pointer dark:bg-dark3"
-              onClick={togglePollModal}
-            >
-              <div className="py-2.5 text-center dark:text-white">What do you have in mind?</div>
+        <div className="flex items-center justify-between mt-3 border-gray-100 px-2 max-lg:flex-col dark:border-slate-700 sticky top-16 border bg-white dark:bg-gray-800 z-50 my-5">
+          <nav className="flex gap-0.5 rounded-xl -mb-px text-gray-600 font-medium text-[15px] dark:text-white max-md:w-full max-md:overflow-x-auto">
+            {["All", "Memes", "Polls"].map((tab) => (
+              <button
+                key={tab}
+                onClick={() => handleTabClick(tab)}
+                className={`inline-block py-3 leading-8 px-3.5 ${
+                  activeTab === tab
+                    ? "border-b-2 border-blue-600 text-blue-600 dark:text-blue-400"
+                    : "text-gray-600 dark:text-gray-400"
+                }`}
+              >
+                {tab}
+              </button>
+            ))}
+          </nav>
+        </div>
+        {activeTab === "All" && (
+          <>
+            <CreatePost />
+            {casts && casts.map((item, i) => <PostCards key={i} data={item} />)}
+            {endCursorcasts && (
+              <div className="flex justify-center my-5">
+                <button
+                  onClick={loadmoreCastData}
+                  className="inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
+                  disabled={loadcasts}
+                >
+                  {loadcasts ? (
+                    <div className="w-8 h-8 border-4 border-t-blue-500 border-solid rounded-full animate-spin"></div>
+                  ) : (
+                    "Load More"
+                  )}
+                </button>
+              </div>
+            )}
+          </>
+        )}
+        {activeTab === "Memes" && (
+          <>
+            <CreatePost />
+            {casts && casts.map((item, i) => <PostCards key={i} data={item} />)}
+            {endCursorcasts && (
+              <div className="flex justify-center">
+                <button
+                  onClick={() => loadmoreCastData()}
+                  className="mt-4 font-bold text-base sm:text-lg bg-gradient-to-r text-white px-4 sm:px-6 py-1 sm:py-2 from-ct-blue-dark to-ct-blue-light min-w-[80px] sm:min-w-[100px] text-center rounded-md"
+                >
+                  {loadcasts ? (
+                    <div className="spinner-circular"></div>
+                  ) : (
+                    "Load More"
+                  )}
+                </button>
+              </div>
+            )}
+          </>
+        )}
+        {activeTab === "Polls" && (
+          <>
+            <div className="bg-white rounded-xl shadow-sm md:p-4 p-2 space-y-4 text-sm font-medium border1 dark:bg-dark2">
+              <div className="flex items-center md:gap-3 gap-1">
+                <div
+                  className="flex-1 bg-slate-100 hover:bg-opacity-80 transition-all rounded-lg cursor-pointer dark:bg-dark3"
+                  onClick={togglePollModal}
+                >
+                  <div className="py-2.5 text-center dark:text-white">
+                    What do you have in mind?
+                  </div>
+                </div>
+                <div
+                  className="cursor-pointer hover:bg-opacity-80 p-1 px-1.5 rounded-xl transition-all bg-pink-100/60 hover:bg-pink-100 dark:bg-white/10 dark:hover:bg-white/20"
+                  onClick={togglePollModal}
+                >
+                  <IonIcon
+                    icon={imageOutline}
+                    className="w-8 h-8 stroke-pink-600 fill-pink-200/70"
+                  />
+                </div>
+                <div
+                  className="cursor-pointer hover:bg-opacity-80 p-1 px-1.5 rounded-xl transition-all bg-sky-100/60 hover:bg-sky-100 dark:bg-white/10 dark:hover:bg-white/20"
+                  onClick={togglePollModal}
+                >
+                  <IonIcon
+                    icon={videocamOutline}
+                    className="w-8 h-8 stroke-sky-600 fill-sky-200/70"
+                  />
+                </div>
+              </div>
             </div>
+            {/* <PostCards /> */}
+          </>
+        )}
+        {showPollModal && (
+          <div className="fixed inset-0 z-[99] flex items-center justify-center">
             <div
-              className="cursor-pointer hover:bg-opacity-80 p-1 px-1.5 rounded-xl transition-all bg-pink-100/60 hover:bg-pink-100 dark:bg-white/10 dark:hover:bg-white/20"
+              className="fixed inset-0 bg-gray-500 bg-opacity-10 backdrop-blur-sm"
               onClick={togglePollModal}
-            >
-              <IonIcon icon={imageOutline} className="w-8 h-8 stroke-pink-600 fill-pink-200/70" />
-            </div>
-            <div
-              className="cursor-pointer hover:bg-opacity-80 p-1 px-1.5 rounded-xl transition-all bg-sky-100/60 hover:bg-sky-100 dark:bg-white/10 dark:hover:bg-white/20"
-              onClick={togglePollModal}
-            >
-              <IonIcon icon={videocamOutline} className="w-8 h-8 stroke-sky-600 fill-sky-200/70" />
-            </div>
+            ></div>
+            <PollInputForm togglePollModal={togglePollModal} />
           </div>
-        </div>
-        {/* <PostCards /> */}
-      </>}
-      {showPollModal && (
-        <div className="fixed inset-0 z-[99] flex items-center justify-center">
-          <div className="fixed inset-0 bg-gray-500 bg-opacity-10 backdrop-blur-sm" onClick={togglePollModal}></div>
-          <PollInputForm togglePollModal={togglePollModal} />
-        </div>
-      )}
-    </div>
+        )}
+      </div>
     </PullToRefresh>
   );
 };
--- src/components/profile/Profile.js
+++ src/components/profile/Profile.js
@@ -1,35 +1,83 @@
-'use server'
-import React  from 'react'; 
-import PostCards from '../posts/PostCards';
-import PostCardLoader from '../loader/PostCardLoader'; 
-import StickyTabs from './StickyTabs'; 
-import ProfileHeader from './ProfileCard'; 
-import MutualFriends from '../sidebar/MutualFriends';
-import { getFeedByHash } from '@/lib/farcaster';
+"use client";
+import React, { useEffect, useState } from "react";
+import PostCards from "../posts/PostCards";
 
-const Profile = async({ user }) => { 
-   const feed = await  getFeedByHash(user.fid);  
-    return (
-        <main id="site__main" className="relative 2xl:ml-[--w-side]  xl:ml-[--w-side-sm] p-2.5 h-[calc(100vh-var(--m-top))] mt-[--m-top]">
-             <div
-                className="lg:flex 2xl:gap-12 gap-8 max-w-[1065px]"
-                id="js-oversized"
-            >
-                <div className="max-w-[1080px] ">
-                    <div className="md:w-[580px] md:max-w-[580px] flex-1 xl:space-y-6 relative">
-                        <ProfileHeader userData={user} />
-                        <StickyTabs /> 
-                        {
-                            feed && feed.map((item,i) => <PostCards data={item} key={i} />)
-                        }
+import PostCardLoader from "../loader/PostCardLoader";
+import StickyTabs from "./StickyTabs";
+import ProfileHeader from "./ProfileCard";
+import MutualFriends from "../sidebar/MutualFriends";
+import { getFeedByHash, getReactions } from "@/lib/farcaster";
 
-                        <PostCardLoader />
-                    </div>
-                </div>
-                 <MutualFriends fid={user}/> 
-            </div>
-        </main>
-    );
+const tabs = ["Feed", "Replies", "Likes"];
+
+const Profile = ({ user }) => {
+  const [activeTab, setActiveTab] = useState("Feed");
+  const [feed, setFeed] = useState([]);
+  const [recasts, setRecasts] = useState([]);
+  const [likes, setLikes] = useState([]);
+
+  useEffect(() => {
+    getProfiledata();
+  }, []);
+
+  const getProfiledata = async () => {
+    const feed = await getFeedByHash(user.fid);
+    setFeed(feed);
+    const recasts = await getReactions(user.fid, "recasts");
+    setRecasts(recasts);
+    const likes = await getReactions(user.fid, "likes");
+    setLikes(likes);
+  };
+
+  return (
+    <main
+      id="site__main"
+      className="relative 2xl:ml-[--w-side]  xl:ml-[--w-side-sm] p-2.5 h-[calc(100vh-var(--m-top))] mt-[--m-top]"
+    >
+      <div
+        className="lg:flex 2xl:gap-12 gap-8 max-w-[1065px]"
+        id="js-oversized"
+      >
+        <div className="max-w-[1080px] ">
+          <div className="md:w-[580px] md:max-w-[580px] flex-1 xl:space-y-6 relative">
+            <ProfileHeader userData={user} />
+            <StickyTabs
+              tabs={tabs}
+              setActiveTab={setActiveTab}
+              activeTab={activeTab}
+            />
+            {activeTab === "Feed" && (
+              <>
+                {feed &&
+                  feed.map((item, i) => <PostCards data={item} key={i} />)}
+              </>
+            )}
+
+            {activeTab === "Replies" && (
+              <>
+                {recasts &&
+                  recasts.map((item, i) => (
+                    <PostCards data={item.cast} key={i} />
+                  ))}
+              </>
+            )}
+
+            {activeTab === "Likes" && (
+              <>
+                {likes &&
+                  likes.map((item, i) => (
+                    <PostCards data={item.cast} key={i} />
+                  ))}
+              </>
+            )}
+
+            <PostCardLoader />
+          </div>
+        </div>
+        <MutualFriends fid={user} />
+      </div>
+    </main>
+  );
 };
 
 export default Profile;
