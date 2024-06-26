9da2b3fa13b139653cb384e2e749d685677c632d
mansijoshi17 <mansijoshi1777@gmail.com>
2024-06-26 09:12:22 +0530

    Removed unused changes

--- src/app/superplay/page.js
+++ src/app/superplay/page.js
@@ -3,74 +3,9 @@ import Layout from "@/components/layout/Layout";
 import RightSIdeBar from "@/components/sidebar/RightSIdeBar";
 import GameCard from "@/components/superplay/GameCard";
 import { gamesData } from "@/lib/utils";
-import axios from "axios";
-import { ethers } from "ethers";
-import React, { useEffect } from "react";
+import React from "react";
 
 const page = () => {
-  const handleChnageBase = async () => {
-    const signer = await window.ethereum;
-    console.log(signer, "signersigner");
-    const currentUser = "0x3Fe0ab910eA2f59D4E7ee7375FA69Acff238B798";
-    const d = {
-      currentUser,
-      signer,
-    };
-    try {
-      const response = await fetch("http://localhost:6000/nft-base-checker", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(d),
-      });
-
-      if (!response.ok) {
-        throw new Error("Network response was not ok");
-      }
-
-      const data = await response.json();
-      console.log(data, "data");
-      return data.eligible;
-    } catch (error) {
-      console.error("Error calling API:", error);
-      throw error;
-    }
-  };
-
-  useEffect(() => {
-    connectMetaMaskAndGetSigner();
-  }, []);
-
-  async function connectMetaMaskAndGetSigner() {
-    if (typeof window != "undefined") {
-      const { ethereum } = window;
-
-      if (!ethereum) {
-        toast.error("Please install the Metamask Extension!");
-      }
-      try {
-        const accounts = await ethereum.request({
-          method: "eth_requestAccounts",
-        });
-        console.log("Found an account! Address: ", accounts[0]);
-        console.log(accounts);
-      } catch (err) {
-        console.log(err);
-        if (err.code === 4902) {
-          try {
-            const accounts = await ethereum.request({
-              method: "eth_requestAccounts",
-            });
-            console.log(accounts);
-          } catch (err) {
-            alert(err.message);
-          }
-        }
-      }
-    }
-  }
-
   return (
     <Layout>
       <main
@@ -92,7 +27,7 @@ const page = () => {
                 </div>
               ))}
             </div>
-            <button onClick={handleChnageBase}>call</button>
+           
           </div>
           <RightSIdeBar />
         </div>
